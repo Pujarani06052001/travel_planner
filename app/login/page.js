bd47aecf3198874c9d87719830b245770f84790a
poojarani1234 <ranipooja21@navgurukul.org>
2024-07-20 12:42:08 +0530

    login and sighup

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,44 @@
-const Login = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
-            <p>Login to your account.</p>
-        </div>
-    );
-};
+"use client"
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Login successful!');
+    // You can add more logic here to handle the login process
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Enter email"
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Enter password"
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
 
 export default Login;
